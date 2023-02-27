926fe173d2165caf27d108d20a9a17c258fdd349
Vinches21 <Vinches2193@yandex.ru>
2023-02-27 14:32:36 +0300

    update elements tests added tests: -test_web_table_add_person -test_web_table_search_person

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
@@ -6,7 +8,12 @@ faker_ru = Faker('ru_RU')
 
 def generated_person():
     yield Person(
-        full_name= faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
+        full_name=faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
+        firstname=faker_ru.first_name(),
+        lastname=faker_ru.last_name(),
+        age=random.randint(10, 80),
+        salary= random.randint(50000, 100000),
+        department=faker_ru.job(),
         email= faker_ru.email(),
         current_address= faker_ru.address(),
         permanent_address= faker_ru.address()
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,48 +1,75 @@
+import random
 import time
 
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
 
 
-class TestElements:
-    class TestTextBox:
+# class TestElements:
+#     class TestTextBox:
+#
+#         def test_test_box(self, driver):
+#             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+#             text_box_page.open()
+#             full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+#             output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
+#             assert full_name == output_name, "the full_name does not match"
+#             assert email == output_email, "the email does not match"
+#             assert current_address == output_cur_addr, "the current_address does not match"
+#             assert permanent_address == output_per_addr,  "the permanent_address does not match"
+#
+# class TestCheckBox:
+#     def test_check_box(self, driver):
+#         check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox') #https://demoqa.com/checkbox
+#         check_box_page.open()
+#         check_box_page.open_full_list()
+#         check_box_page.click_random_checkbox()
+#         input_checkbox = check_box_page.get_checked_checkboxes()
+#         output_result = check_box_page.get_output_result()
+#         print(input_checkbox)
+#         print(output_result)
+#         assert input_checkbox == output_result, 'Checkboxes have not been selected'
+#
+#
+# class TestRadioButton:
+#
+#     def test_radio_button(self, driver):
+#         radio_button_page = RadioButtonPage(driver, 'https://demoqa.com/radio-button')
+#         radio_button_page.open()
+#         radio_button_page.click_radio_button('yes')
+#         output_yes = radio_button_page.get_output_result()
+#         radio_button_page.click_radio_button('impressive')
+#         output_impressive = radio_button_page.get_output_result()
+#         radio_button_page.click_radio_button('no')
+#         output_no = radio_button_page.get_output_result()
+#         assert output_yes == 'Yes', 'Yes have been selected'
+#         assert output_impressive == 'Impressive', 'Impressive have been selected'
+#         assert output_no == 'No', 'No have been selected'
 
-        def test_test_box(self, driver):
-            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
-            text_box_page.open()
-            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
-            output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
-            assert full_name == output_name, "the full_name does not match"
-            assert email == output_email, "the email does not match"
-            assert current_address == output_cur_addr, "the current_address does not match"
-            assert permanent_address == output_per_addr,  "the permanent_address does not match"
+class TestWebTable:
 
-class TestCheckBox:
-    def test_check_box(self, driver):
-        check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox') #https://demoqa.com/checkbox
-        check_box_page.open()
-        check_box_page.open_full_list()
-        check_box_page.click_random_checkbox()
-        input_checkbox = check_box_page.get_checked_checkboxes()
-        output_result = check_box_page.get_output_result()
-        print(input_checkbox)
-        print(output_result)
-        assert input_checkbox == output_result, 'Checkboxes have not been selected'
+    def test_web_table_add_person(self, driver):
+        web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+        web_table_page.open()
+        new_person = web_table_page.add_new_person()
+        table_result = web_table_page.check_new_added_person()
+        print(new_person)
+        print(table_result)
+        assert new_person in table_result
+        time.sleep(5)
+
+    def test_web_table_search_person(self, driver):
+        web_table_page = WebTablePage(driver, 'https://demoqa.com/webtables')
+        web_table_page.open()
+        key_word = web_table_page.add_new_person()[random.randint(0, 5)]
+        web_table_page.search_some_person(key_word)
+        table_result = web_table_page.check_search_person()
+        time.sleep(2)
+        assert key_word in table_result, 'the person was not found in the table'
 
 
-class TestRadioButton:
 
-    def test_radio_button(self, driver):
-        radio_button_page = RadioButtonPage(driver, 'https://demoqa.com/radio-button')
-        radio_button_page.open()
-        radio_button_page.click_radio_button('yes')
-        output_yes = radio_button_page.get_output_result()
-        radio_button_page.click_radio_button('impressive')
-        output_impressive = radio_button_page.get_output_result()
-        radio_button_page.click_radio_button('no')
-        output_no = radio_button_page.get_output_result()
-        assert output_yes == 'Yes', 'Yes have been selected'
-        assert output_impressive == 'Impressive', 'Impressive have been selected'
-        assert output_no == 'No', 'No have been selected'
+
+
 
 
 
