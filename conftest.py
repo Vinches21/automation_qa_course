cc69fe3cb65ddb47328e3f25c29d7f6b02f8aba0
Oleg Polikarpov <Vinches2193@yandex.ru>
2023-07-24 23:04:53 +0300

    add firefox driver

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest as pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import  GeckoDriverManager
 
 
 
@@ -13,3 +14,9 @@ def driver():
     yield driver
     driver.quit()
 
+@pytest.fixture(scope="function")
+def driver_firefox():
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
