f09a2217503d32b4c1f4dad91171aed1b9df0fd4
Oleg Polikarpov <Vinches2193@yandex.ru>
2023-08-01 15:49:25 +0300

    change branch

--- pages/base_page.py
+++ pages/base_page.py
@@ -12,7 +12,7 @@ class BasePage:
         self.driver.get(self.url)
 
     """Метод для поиска видимого элемента"""
-    def element_is_visible(self, locator, timeout=5):
+    def elements_is_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
     """Метод для поиска видимовых элементов"""
@@ -20,7 +20,7 @@ class BasePage:
         return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
 
     """Метод для поиска элемента(текст к примеру) из дом-дерева"""
-    def element_is_present(self, locator, timeout=5):
+    def elements_is_present(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
 
     def elements_are_present(self, locator, timeout=5):
