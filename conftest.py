02e318500cb939efbac275975ade0b359644f1be
Oleg Polikarpov <Vinches2193@yandex.ru>
2023-08-22 23:20:28 +0300

    correct driver chrome

--- conftest.py
+++ conftest.py
@@ -1,18 +1,24 @@
 import pytest as pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.firefox import GeckoDriverManager
-
-
-
 
 
 @pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
     yield driver
     driver.quit()
+
+
+# @pytest.fixture(scope="function")
+# def driver():
+#     driver = webdriver.Chrome()
+#     driver.maximize_window()
+#     yield driver
+#     driver.quit()
 
 @pytest.fixture(scope="function")
 def driver_firefox():
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -158,10 +158,9 @@ class TestPracticePage:
     def test_practice_form(self, driver):
         practice_form = PracticePage(driver, "https://demoqa.com/automation-practice-form")
         practice_form.open()
-        person_info = practice_form.check_practice_form()
+        p = practice_form.check_practice_form()
         result = practice_form.form_result()
-        print(person_info)
-        print(result)
+        assert [p.firstname + " " + p.lastname, p.email] == [result[0], result[1]], "Не совпадают данные"
 
 
 
