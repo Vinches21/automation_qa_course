4b18d349f3d58e22afadba50c77cf64a6a0def05
Oleg Polikarpov <Vinches2193@yandex.ru>
2024-01-23 22:39:58 +0300

    allerts

--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,7 +1,7 @@
 import time
 from random import randint as rd
 
-from pages.alerts_frame_windows_pages import BrowserWindowsPage
+from pages.alerts_frame_windows_pages import BrowserWindowsPage, AlertsPage
 
 
 class TestAlertsFrameWindow:
@@ -15,4 +15,28 @@ class TestAlertsFrameWindow:
             assert text_result == "This is a sample page", "The new tab has not opened or incorrect tab has opened"
 
 
+    class TestAlertsPage():
 
+        def test_see_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_see_alert()
+            assert alert_text == 'You clicked a button', 'Alert did not show up'
+
+        def test_alert_appear_5_sec(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_alert_appear_5_sec()
+            assert alert_text == 'This alert appeared after 5 seconds', 'Alert did not show up'
+
+        def test_confirm_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_confirm_alert()
+            assert alert_text == 'You selected Ok', 'Alert did not show up'
+
+        def test_prompt_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text, alert_text = alert_page.check_prompt_alert()
+            assert text in alert_text, 'Alert did not show up'
